ee459ebf6375f7258b3ffd9b26517994ab93e197
stormyCobra1199 <102003206+stormyCobra1199@users.noreply.github.com>
2022-05-08 15:47:18 +0800

    Add files via upload
    added save search results to gist

--- resaleHDBpx.py
+++ resaleHDBpx.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime
 import logging
 import requests
+import json
 import re
 import os
 import telegram
@@ -11,12 +12,13 @@ from pandas.plotting import table
 
 PORT = int(os.environ.get('PORT', 5000))
 TOKEN = os.environ["TOKEN"]
+GTOKEN = os.environ["GTOKEN"]
+GID = os.environ["GID"]
 
 def get_url():
     contents = requests.get('https://dog.ceo/api/breeds/image/random').json()
     url = contents['message']
     return url
-
 
 def get_image_url():
     allowed_extension = ['jpg', 'jpeg', 'png']
@@ -26,7 +28,6 @@ def get_image_url():
         file_extension = re.search("([^.]*)$", url).group(1).lower()
     return url
 
-
 def bop(update, context):
     url = get_image_url()
     chat_id = update.message.chat.id
@@ -35,9 +36,6 @@ def bop(update, context):
 
 ###
 def start(update, context):
-    outF = open("trackStart.txt", "a")
-    outF.write("1 ")
-    outF.close()
     context.bot.send_message(chat_id=update.effective_chat.id, text="HDBRT Bot - Get latest HDB Resale Transactions.\nExamples: /hs 5 Bedok for 5Rm in Bedok or /hs French for all French Road.")
    
 def hs(update, context):
@@ -57,10 +55,11 @@ def hs(update, context):
     df.drop('model', axis=1, inplace=True)
     dft=tabulate(df, headers='keys', tablefmt='plain', numalign='left')
     context.bot.send_message(chat_id=update.effective_chat.id, text=dft)
-    ct = datetime.datetime.now()
-    outF = open(str(ct), "w")
-    outF.write(dft)
-    outF.close()
+    ## copy search results to gist
+    query_url = "https://api.github.com/gists/"+GID+"/comments"
+    data = {"body": dft}
+    headers = {'Authorization': f'token {GTOKEN}'}
+    r = requests.post(query_url, headers=headers, data=json.dumps(data))
 
 def errorhand(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter a valid town or search term. Eg: /hs 5 Bedok for 5Rm in Bedok or /hs French for all French Road.")
@@ -68,12 +67,11 @@ def errorhand(update, context):
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
-
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('bop', bop))
     dp.add_handler(CommandHandler('hs', hs))
     dp.add_error_handler(errorhand)
-    
+    ##
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
